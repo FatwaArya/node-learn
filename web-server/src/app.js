836f822c4a3d032d487344dc408dbb2e1a262a30
FatwaArya <fatwaaryasatyaakbar@gmail.com>
2021-12-21 21:31:49 +0700

    using express and hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,44 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
- app.get('',(req, res)=>{
-    res.send('Hello Express!')
- })
+//define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewspath = path.join(__dirname, '../template')
 
-app.get('/help' , (req ,res)=>{
-    res.send('help page')
+//setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views', viewspath)
+//setup static dir to server css img and js
+app.use(express.static(publicDirectoryPath))
+
+//
+app.get('',(reg, res)=>{
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Fatwa Arya'
+    })
 })
-app.get('/about', (req, res)=>{
-    res.send('about page')
+app.get('/about', (req,res)=>{
+    res.render('about',{
+        title: 'About me',
+        name: 'Fatwa Arya'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title: 'Help Page',
+        name: 'Fatwa Arya'
+    })
 })
 app.get('/weather', (req, res)=>{
-    res.send('weather page')
+    res.send({
+        location: 'Blitar',
+        forecast: 'Rainy'
+    })
 })
-app.get
+
  app.listen(3000, ()=>{
      console.log('Server is up on port 3000');
  })
