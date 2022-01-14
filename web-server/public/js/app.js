cf3a3dc26ce44b32dcf09a74f0836a2b754fa725
FatwaArya <fatwaaryasatyaakbar@gmail.com>
2022-01-14 10:46:25 +0700

    test-ssh-key

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,6 @@
 console.log("client side js loaded");
 // dont forget to use this to execute the program
-//nodemon src/app.js -e js,hbs
+//nodemon src/app.js -e js,hbss
 
 // fetch('https://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
@@ -32,11 +32,7 @@ weatherForm.addEventListener('submit', (e) => {
             } else {
                 msg1.textContent = data.location
                 msg2.textContent = data.forecast
-
-
-
             }
-
         })
     })
 
